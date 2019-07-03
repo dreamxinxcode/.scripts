6daf122c30cc4d6a7bd30fa42a1d123d22b92ef0
Brandon Lecky <42306596+dreamxinxcode@users.noreply.github.com>
2019-07-02 22:41:06 -0700

    Add files via upload

---
+++ cyberpunk.sh
@@ -0,0 +1,5 @@
+
+bspc config border_width           4
+bspc config presel_feedback_color  "#666666"
+bspc config normal_border_color    "#333333"
+bspc config focused_border_color   "#555555"
---
+++ walcycle.sh
@@ -0,0 +1,18 @@
+#! /bin/sh
+#========================================#
+#     Caligula's wal recycle script.     #
+#========================================#
+
+# Cycle wal background image and colorscheme.
+wal -i "$HOME/Pictures/wall"
+
+# Restart polybar to match new colorscheme.
+pkill polybar
+polybar mybar &
+
+# Refresh bspwm's colorscheme.
+source "$HOME/.cache/wal/colors.sh"
+bspc config normal_border_color $color0
+bspc config focused_border_color $color1
+bspc config active_border_color $color1
+bspc config presel_feedback_color $color1
