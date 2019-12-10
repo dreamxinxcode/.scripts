2a56e74ce382a1e4952e8705a989e3b526dffa8c
dreamxinxcode <dreamxinxcode@gmail.com>
2019-12-10 02:33:27 +0000

    Created 'add-to-useful.py'

--- add-to-useful.py
+++ add-to-useful.py
@@ -1,10 +1,13 @@
 # I keep a file in my Documents where I store useful snippets.
 # This script is to quickly add a snippet to that file.
 
+import os
+
 def main():
     description = input("Description: ")
     snippet = input("Snippet: ")
-    with open("../Documents/useful", "a") as f:
+    home = os.environ["HOME"]
+    with open(home + "/Documents/useful", "a") as f:
         f.write("\n# " + description + "\n" + snippet)
         f.close
 if (__name__ == "__main__"):
