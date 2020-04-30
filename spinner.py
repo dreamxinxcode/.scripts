42dfc7e58ba423330b6e05a3f95bc09c7533cd71
dreamxinxcode <dreamxinxcode@gmail.com>
2020-04-30 18:33:06 +0000

    Added 'productive-mode.py'

--- spinner.py
+++ spinner.py
@@ -2,15 +2,15 @@ import time
 import sys
 
 while True:
-    sys.stdout.write("\\\r")
+    sys.stdout.write("\\\\\r")
     time.sleep(.25)
     sys.stdout.flush()
-    sys.stdout.write("-\r")
+    sys.stdout.write("--\r")
     sys.stdout.flush()
-    sys.stdout.write("/\r")
+    sys.stdout.write("//\r")
     time.sleep(.25)
     sys.stdout.flush()
-    sys.stdout.write("-\r")
+    sys.stdout.write("--\r")
     time.sleep(.25)
     sys.stdout.flush()
 
