d70b9f24ce12257e66ba568e53233f50d3417b1b
dreamxinxcode <brandon@dreamincode.dev>
2022-04-17 02:53:58 +0000

    04/16/2022

--- cyberpunk.sh
+++ cyberpunk.sh
@@ -1,5 +1,2 @@
-
-bspc config border_width           4
-bspc config presel_feedback_color  "#666666"
-bspc config normal_border_color    "#333333"
-bspc config focused_border_color   "#555555"
+wal -f /home/$USER/.colors/cyberpunk.json
+feh --bg-scale /home/$USER/Pictures/wal/wall7.jpg
